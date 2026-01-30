8adf2fe36ae06de1a73e05e7540da0f07f71af14
techmuse <44119098+techmuse8@users.noreply.github.com>
2026-01-30 11:05:31 -0500

    Aroma Telkin launcher rework
    - The plugin should now be mostly game-agnostic,; After doing some testing a call_ctors hook isn't needed in order to run the Telkin bootstrap
    - As a result libfunctionpatcher and rplinfo aren't required anymore, so they have been removed
    - Re-enable the RPL loading code
    - Make sure that the RPL is unloaded on app exit
    - Added plugin description
    - More descriptive var names and logging

--- launcher/aroma/source/main.cpp
+++ launcher/aroma/source/main.cpp
@@ -6,55 +6,49 @@
 #include <whb/log_module.h>
 #include <whb/log_cafe.h>
 #include <whb/log_udp.h>
-#include <function_patcher/function_patching.h>
+
 #include <content_redirection/redirection.h>
+#include <kernel/kernel.h>
 
 #include <coreinit/cache.h>
+#include <coreinit/dynload.h>
 #include <coreinit/title.h>
 
-#include <patcher/rplinfo.h>
-
 WUPS_PLUGIN_NAME("Telkin RPL Loader");
-WUPS_PLUGIN_DESCRIPTION("<to be written>");
+WUPS_PLUGIN_DESCRIPTION("A dynamic Wii U mod loader.");
 WUPS_PLUGIN_VERSION("prerelease");
 WUPS_PLUGIN_AUTHOR("techmuse, Luminyx");
 WUPS_PLUGIN_LICENSE("MPL-2.0");
 
 WUPS_USE_WUT_DEVOPTAB();
 
-#define NSMBU_US_TID 0x0005000010101D00llu // TEMP
-static int Initialized = 0;
-static uint32_t TelkinRPLHandle;
+static bool TelkinInitialized = 0;
+OSDynLoad_Module TelkinRPLHandle = nullptr;
 static CRLayerHandle contentLayerHandle;
-
-static PatchedFunctionHandle patchHandle;
 
 void TelkinBootstrap() {
     WHBLogPrintf("In the Telkin bootstrap!\n");
-    if (Initialized)
+    if (TelkinInitialized)
         return;
 
-    typedef void (*TelkinInitFuncHandle_t)(void *acquireAddr, void *exportAddr);
-    TelkinInitFuncHandle_t TelkinInitFuncHandle;
-    // The RPL doesn't load properly on console yet so this is out for now
+    typedef void (*Telkin_init_t)(void *acquireAddr, void *exportAddr);
+    Telkin_init_t Telkin_init;
 
-    /* OSDynLoad_Error err = OSDynLoad_Acquire("~/Telkin.rpl", (OSDynLoad_Module*)&TelkinRPLHandle); // load RPL from SD card
+    OSDynLoad_Error err = OSDynLoad_Acquire("Telkin.rpl", &TelkinRPLHandle); // load RPL from SD card
     if (err != OS_DYNLOAD_OK) {
-        WHBLogPrintf("Couldn't load Telkin.rpl! Err: %08X\n", err);
+        WHBLogPrintf("Couldn't load Telkin.rpl! Err code: %08X\n", err);
+        return;
     }
 
-    OSDynLoad_FindExport((OSDynLoad_Module*)&TelkinRPLHandle, OS_DYNLOAD_EXPORT_FUNC, "init", (void**)TelkinInitFuncHandle);
+    err = OSDynLoad_FindExport(&TelkinRPLHandle, OS_DYNLOAD_EXPORT_FUNC, "init", (void**)&Telkin_init);
+    if (err != OS_DYNLOAD_OK) {
+        WHBLogPrintf("Couldn't find Telkin's 'init' function! Err code: %08X\n", err);
+        return;
+    }
 
-    TelkinInitFuncHandle((void*)&OSDynLoad_Acquire, (void*)&OSDynLoad_FindExport);
-    */
-    return;
-}
-
-DECL_FUNCTION(void, call_ctors) {
-    WHBLogPrintf("In call ctors hook\n");
-    real_call_ctors();
-    WHBLogPrintf("Post real call ctors\n");
-    TelkinBootstrap();
+    Telkin_init((void*)&OSDynLoad_Acquire, (void**)&OSDynLoad_FindExport);
+    TelkinInitialized = true;
+    WHBLogPrintf("Telkin has been initialized!");
     return;
 }
 
@@ -76,77 +70,26 @@ void RedirectContentDir() {
 }
 
 INITIALIZE_PLUGIN() {
-    WHBLogPrintf("Patch functions");
-    if (FunctionPatcher_InitLibrary() != FUNCTION_PATCHER_RESULT_SUCCESS) {
-        OSFatal("Telkin RPL Loader: FunctionPatcher_InitLibrary failed");
-    }
-
     ContentRedirectionStatus error;
     if ((error = ContentRedirection_InitLibrary()) != CONTENT_REDIRECTION_RESULT_SUCCESS) {
         WHBLogPrintf("Failed to init ContentRedirection. Error %s %d", ContentRedirection_GetStatusStr(error), error);
         OSFatal("Failed to init ContentRedirection.");
     }
-
 }
 
 DEINITIALIZE_PLUGIN() {
-    FunctionPatcher_RemoveFunctionPatch(patchHandle);
+  //...
 }
 
 ON_APPLICATION_START() {
-    if (OSGetTitleID() != NSMBU_US_TID) {
-        return;
-    }
-    
     // Init logging
     if (!WHBLogModuleInit()) {
         WHBLogCafeInit();
         WHBLogUdpInit();
     }
 
-    WHBLogPrintf("Telkin: applying patches...");
-    /* 
-    Early attempts (might get removed?)
-
-    // Patch the dynload functions so GetRPLInfo works
-    if (!PatchDynLoadFunctions()) {
-        WHBLogPrintf("Telkin: Failed to patch dynload functions");
-        return;
-    }
-
-    // Get the RPLInfo
-    auto rpl_info = TryGetRPLInfo();
-    if (!rpl_info) {
-        WHBLogPrintf("Telkin: Failed to get RPL info");
-        return;
-    }
-
-    // Find the rpx
-    rplinfo rpls = *rpl_info;
-    auto red_pro2_rpx = FindRPL(rpls, "red-pro2.rpx");
-    if (!red_pro2_rpx) {
-        WHBLogPrintf("Telkin: Failed to find red-pro2.rpx");
-        return;
-    }
-
-    OSDynLoad_NotifyData rpx_data = *red_pro2_rpx;
-    uint32_t inject_addr = rpx_data.textAddr + 0xaf9fb0; 
-    */
-
-    uint64_t titleIds[]              = {NSMBU_US_TID};
-    function_replacement_data_t repl = REPLACE_FUNCTION_OF_EXECUTABLE_BY_ADDRESS_WITH_VERSION(
-            call_ctors,
-            titleIds, sizeof(titleIds) / sizeof(titleIds[0]),
-            "red-pro2.rpx",
-            0xaf9fd0, // Address of 'call_ctors'
-            64, 64);
-
-    if (FunctionPatcher_AddFunctionPatch(&repl, &patchHandle, nullptr) != FUNCTION_PATCHER_RESULT_SUCCESS) {
-        WHBLogPrintf("Failed to add \"call_ctors\" patch\n");
-    }
-
     RedirectContentDir();
-
+    TelkinBootstrap();
 }
 
 ON_APPLICATION_ENDS() {
@@ -155,4 +98,6 @@ ON_APPLICATION_ENDS() {
         contentLayerHandle = 0;
     }
 
+    OSDynLoad_Release(TelkinRPLHandle);
+    TelkinRPLHandle = nullptr;
 }
