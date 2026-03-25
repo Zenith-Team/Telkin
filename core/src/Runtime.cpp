0e378ed817a70cb88804d776aff53033fef1dd3d
Luminyx <27lumi@protonmail.com>
2026-03-25 19:22:41 -0400

    Runtime: add islower/toupper

--- core/src/Runtime.cpp
+++ core/src/Runtime.cpp
@@ -350,6 +350,51 @@ extern "C" double atof(const char *s) {
     return a * sign;
 }
 
+/* String stdlib functions */
+
+// toupper
+#define _U 01
+#define _L 02
+#define _N 04
+#define _S 010
+#define _P 020
+#define _C 040
+#define _X 0100
+#define _B 0200
+
+#define _CTYPE_DATA_0_127                                                     \
+    _C, _C, _C, _C, _C, _C, _C, _C, _C, _C | _S, _C | _S, _C | _S, _C | _S,   \
+        _C | _S, _C, _C, _C, _C, _C, _C, _C, _C, _C, _C, _C, _C, _C, _C, _C,  \
+        _C, _C, _C, _S | _B, _P, _P, _P, _P, _P, _P, _P, _P, _P, _P, _P, _P,  \
+        _P, _P, _P, _N, _N, _N, _N, _N, _N, _N, _N, _N, _N, _P, _P, _P, _P,   \
+        _P, _P, _P, _U | _X, _U | _X, _U | _X, _U | _X, _U | _X, _U | _X, _U, \
+        _U, _U, _U, _U, _U, _U, _U, _U, _U, _U, _U, _U, _U, _U, _U, _U, _U,   \
+        _U, _U, _P, _P, _P, _P, _P, _P, _L | _X, _L | _X, _L | _X, _L | _X,   \
+        _L | _X, _L | _X, _L, _L, _L, _L, _L, _L, _L, _L, _L, _L, _L, _L, _L, \
+        _L, _L, _L, _L, _L, _L, _L, _P, _P, _P, _P, _C
+
+#define _CTYPE_DATA_128_255                                                    \
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, \
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   \
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   \
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   \
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   \
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+
+const char _ctype_[1 + 256] = {0, _CTYPE_DATA_0_127,
+                                        _CTYPE_DATA_128_255};
+
+#define __locale_ctype_ptr() _ctype_
+#define __CTYPE_PTR (__locale_ctype_ptr())
+
+extern "C" int islower(int c) {
+    return ((__CTYPE_PTR[c + 1] & (_U | _L)) == _L);
+}
+
+extern "C" int toupper(int c) {
+    return islower(c) ? c - 'a' + 'A' : c;
+}
+
 // from compiler-rt
 
 extern "C" u64 __fixunssfdi(float a) {
