8cb3e1e4555edca4885ce52545bd41dd25d8be4f
Luminyx <27lumi@protonmail.com>
2026-03-25 19:20:30 -0400

    Update Runtime.cpp

--- core/src/Runtime.cpp
+++ core/src/Runtime.cpp
@@ -2,6 +2,7 @@
 #include <cafe.h>
 #include <cstdarg>
 #include <climits>
+#include <cmath>
 #include <limits>
 
 #define sprintf(str, format, ...) \
@@ -26,6 +27,21 @@ void operator delete(void* ptr) noexcept {
 
 extern "C" void free(void* ptr) {
     MEMFreeToDefaultHeap(ptr);
+}
+
+extern "C" void* memalign(size_t align, size_t size) {
+    return MEMAllocFromDefaultHeapEx(size, align);
+}
+
+extern "C" void* memchr(const void* p, int ch, size_t count) {
+    for (size_t i = 0; i < count; i++) {
+        const u8 b = reinterpret_cast<const u8*>(p)[i];
+        if (b == (u8)ch) {
+            return (void*) (&reinterpret_cast<const u8*>(p)[i]);
+        }
+    }
+
+    return nullptr;
 }
 
 extern "C" void* calloc(size_t num, size_t size) {
@@ -231,3 +247,257 @@ extern "C" int strncmp(const char* s1, const char* s2, size_t n) {
     }
     return (*(unsigned char*)s1) - (*(unsigned char*)s2);
 }
+
+extern "C" char* strncpy(char* __restrict dst0, const char* __restrict src0, size_t count) {
+    char* dscan;
+    const char* sscan;
+
+    dscan = dst0;
+    sscan = src0;
+    while (count > 0) {
+        --count;
+        if ((*dscan++ = *sscan++) == '\0') break;
+    }
+    while (count-- > 0) *dscan++ = '\0';
+    return dst0;
+}
+
+extern "C" char* strstr(const char* hs, const char* ne) {
+    size_t i;
+    int c = ne[0];
+
+    if (c == 0) return (char*)hs;
+
+    for (; hs[0] != '\0'; hs++) {
+        if (hs[0] != c) continue;
+        for (i = 1; ne[i] != 0; i++)
+            if (hs[i] != ne[i]) break;
+        if (ne[i] == '\0') return (char*)hs;
+    }
+    return NULL;
+}
+
+/* Math cstdlib funcs*/
+
+extern "C" float fabsf(float a) {
+    return (a > 0) ? a : -a;
+}
+
+extern "C" double fabs(double a) {
+    return (a > 0) ? a : -a;
+}
+
+extern "C" double atof(const char *s) {
+    double a = 0.0;
+    int e = 0;
+    int c;
+    int sign = 1;
+
+    // Skip whitespace
+    while (*s == ' ' || *s == '\t' || *s == '\n') {
+        s++;
+    }
+
+    // Handle sign
+    if (*s == '+') {
+        s++;
+    } else if (*s == '-') {
+        sign = -1;
+        s++;
+    }
+
+    while ((c = *s++) != '\0' && isdigit(c)) {
+        a = a * 10.0 + (c - '0');
+    }
+
+    if (c == '.') {
+        while ((c = *s++) != '\0' && isdigit(c)) {
+            a = a * 10.0 + (c - '0');
+            e = e - 1;
+        }
+    }
+
+    if (c == 'e' || c == 'E') {
+        int exp_sign = 1;
+        int i = 0;
+        c = *s++;
+
+        if (c == '+') {
+            c = *s++;
+        } else if (c == '-') {
+            c = *s++;
+            exp_sign = -1;
+        }
+
+        while (isdigit(c)) {
+            i = i * 10 + (c - '0');
+            c = *s++;
+        }
+
+        e += i * exp_sign;
+    }
+
+    while (e > 0) {
+        a *= 10.0;
+        e--;
+    }
+
+    while (e < 0) {
+        a *= 0.1;
+        e++;
+    }
+
+    return a * sign;
+}
+
+// from compiler-rt
+
+extern "C" u64 __fixunssfdi(float a) {
+    if (a <= 0.0f)
+        return 0;
+    double da = a;
+    u32 high = da / 4294967296.f;               // da / 0x1p32f;
+    u32 low = da - (double)high * 4294967296.f; // high * 0x1p32f;
+    return ((u64)high << 32) | low;
+}
+
+extern "C" s64 __fixsfdi(float a) {
+    if (a < 0.0f) {
+        return -__fixunssfdi(-a);
+    }
+    return __fixunssfdi(a);
+}
+
+extern "C" u64 __fixunsdfdi(double a) {
+    if (a <= 0.0)
+        return 0;
+    u32 high = a / 4294967296.f;               // a / 0x1p32f;
+    u32 low = a - (double)high * 4294967296.f; // high * 0x1p32f;
+    return ((u64)high << 32) | low;
+}
+
+extern "C" s64 __fixdfdi(double a) {
+    if (a < 0.0) {
+        return -__fixunsdfdi(-a);
+    }
+    return __fixunsdfdi(a);
+}
+
+extern "C" double __floatdidf(s64 a) {
+    static const double twop52 = 4503599627370496.0; // 0x1.0p52
+    static const double twop32 = 4294967296.0;       // 0x1.0p32
+
+    union {
+        int64_t x;
+        double d;
+    } low = {.d = twop52};
+
+    const double high = (int32_t)(a >> 32) * twop32;
+    low.x |= a & 0x00000000ffffffffLL;
+
+    const double result = (high - twop52) + low.d;
+    return result;
+}
+
+extern "C" float __floatdisf(s64 a) {
+    if (a == 0)
+        return 0.0;
+
+    enum {
+        dstMantDig = 23 + 1,
+        srcBits = sizeof(s64) * 8,
+        srcIsSigned = ((s64)-1) < 0,
+    };
+
+    const s64 s = srcIsSigned ? a >> (srcBits - 1) : 0;
+
+    a = (u64)(a ^ s) - s;
+    int sd = srcBits - __builtin_clzll(a); // number of significant digits
+    int e = sd - 1;                        // exponent
+    if (sd > dstMantDig) {
+        //  start:  0000000000000000000001xxxxxxxxxxxxxxxxxxxxxxPQxxxxxxxxxxxxxxxxxx
+        //  finish: 000000000000000000000000000000000000001xxxxxxxxxxxxxxxxxxxxxxPQR
+        //                                                12345678901234567890123456
+        //  1 = msb 1 bit
+        //  P = bit dstMantDig-1 bits to the right of 1
+        //  Q = bit dstMantDig bits to the right of 1
+        //  R = "or" of all bits to the right of Q
+        if (sd == dstMantDig + 1) {
+        a <<= 1;
+        } else if (sd == dstMantDig + 2) {
+        // Do nothing.
+        } else {
+        a = ((u64)a >> (sd - (dstMantDig + 2))) |
+            ((a & ((u64)(-1) >> ((srcBits + dstMantDig + 2) - sd))) != 0);
+        }
+        // finish:
+        a |= (a & 4) != 0; // Or P into R
+        ++a;               // round - this step may add a significant bit
+        a >>= 2;           // dump Q and R
+        // a is now rounded to dstMantDig or dstMantDig+1 bits
+        if (a & ((u64)1 << dstMantDig)) {
+        a >>= 1;
+        ++e;
+        }
+        // a is now rounded to dstMantDig bits
+    } else {
+        a <<= (dstMantDig - sd);
+        // a is now rounded to dstMantDig bits
+    }
+    const int dstBits = sizeof(float) * 8;
+    const u32 dstSignMask = 1U << (dstBits - 1);
+    const int dstExpBits = dstBits - 23 - 1;
+    const int dstExpBias = (1 << (dstExpBits - 1)) - 1;
+    const u32 dstSignificandMask = (1U << 23) - 1;
+    // Combine sign, exponent, and mantissa.
+    const u32 result = ((u32)s & dstSignMask) |
+                        ((u32)(e + dstExpBias) << 23) |
+                        ((u32)(a)&dstSignificandMask);
+
+    const union {
+        float f;
+        u32 i;
+    } rep = {.i = result};
+    return rep.f;
+}
+
+extern "C" float __floatundisf(u64 a) {
+    return __floatdisf(static_cast<s64>(a));
+}
+
+extern "C" double __floatundidf(u64 a) {
+    static const double twop52 = 4503599627370496.0;           // 0x1.0p52
+    static const double twop84 = 19342813113834066795298816.0; // 0x1.0p84
+    static const double twop84_plus_twop52 =
+        19342813118337666422669312.0; // 0x1.00000001p84
+
+    union {
+        uint64_t x;
+        double d;
+    } high = {.d = twop84};
+    union {
+        uint64_t x;
+        double d;
+    } low = {.d = twop52};
+
+    high.x |= a >> 32;
+    low.x |= a & 0x00000000ffffffffULL;
+
+    const double result = (high.d - twop84_plus_twop52) + low.d;
+    return result;
+}
+
+#define TELKIN_REGISTERS
+#include <telkin/Assembly.h>
+
+extern "C" int printf(const char* format, ...) tAssembly(
+    // r2 is persistent/unused
+    lis r2, OSReport@ha;
+    lwz r2, OSReport@l(r2);
+    mtctr r2;
+    mflr r2;
+    bctrl;
+    mtlr r2;
+    li r3, 0;
+    blr;
+)
