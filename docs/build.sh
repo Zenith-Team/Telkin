a943f250afe602d5ddc9d266e0c4447afa2fc3a4
Luminyx <27lumi@protonmail.com>
2026-06-17 18:47:37 -0400

    docs: Add build.sh

---
+++ docs/build.sh
@@ -0,0 +1,3 @@
+wget https://github.com/doxygen/doxygen/releases/download/Release_1_14_0/doxygen-1.14.0.linux.bin.tar.gz
+tar -xzf doxygen-1.14.0.linux.bin.tar.gz
+./doxygen-1.14.0/bin/doxygen ./docs/Doxyfile
